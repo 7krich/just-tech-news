f87fc1f2f9f896b1806cdfa79ac460a256fc4b09
Kyle Richnafsky <7krich@gmail.com>
2022-04-27 18:17:49 -0400

    set up full sequelize config & file structure

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+// collect packaged group of API endpoints & prefix them wtih api path
+
+const router = require('express').Router();
+
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
