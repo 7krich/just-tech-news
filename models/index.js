e47b953cbe1cb9bbc7213dd25be04b761b31825a
Kyle Richnafsky <7krich@gmail.com>
2022-04-28 13:04:05 -0400

    added relationships for votes in index.js

--- models/index.js
+++ models/index.js
@@ -2,6 +2,8 @@ const User = require('./User');
 
 const Post = require('./Post');
 
+const Vote = require('./Vote');
+
 // create association
 // one to many relationship
 // one user can create many posts
@@ -14,5 +16,36 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
+// voter many to many relationship
+// see which users voted on a single post
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
 
-module.exports = { User, Post };
+// see which posts a single user voted on
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Vote };
