08a692a32509b2ffe60f1c60e4b17c4d01b1766c
Kyle Richnafsky <7krich@gmail.com>
2022-05-04 11:17:51 -0400

    logout added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -166,4 +166,16 @@ router.delete('/:id', (req, res) => {
     });
 });
 
+// allow user to log out if signed in
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else {
+        res.status(404).end();
+    }
+})
+
 module.exports = router;
