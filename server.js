59a2aa4a74bc5272e2932611c69402db64c1ff5e
Kyle Richnafsky <7krich@gmail.com>
2022-05-04 10:55:03 -0400

    added sessions to login's

--- server.js
+++ server.js
@@ -11,7 +11,19 @@ const hbs = exphbs.create({});
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//handlebars
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitalized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 app.use(express.json());
