d218710f8b95242503a9dbdd39ebffe64ab05b0c
Kyle Richnafsky <7krich@gmail.com>
2022-05-12 13:54:59 -0400

    required log in for certain features & updated missing comment routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,11 +31,14 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
         // pass a single post object into the homepage template
         const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts });
+        res.render('homepage', { 
+            posts,
+            loggedIn: req.session.loggedIn
+         });
     })
-        .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
     });
 });
 
