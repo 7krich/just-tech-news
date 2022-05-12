4aa46e9b7964ee011921ab0f0a8c99d82ee2f551
Kyle Richnafsky <7krich@gmail.com>
2022-05-12 13:32:29 -0400

    added upvote functionality

---
+++ public/javascript/upvote.js
@@ -0,0 +1,27 @@
+// handle upvote click
+// click with may asynchronous PUT request to update with fetch()
+async function upvoteClickHandler(event) {
+    event.preventDefault();
+    
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch('/api/posts/upvote', {
+        method: 'PUT',
+        body: JSON.stringify({
+        post_id: id
+        }),
+        headers: {
+        'Content-Type': 'application/json'
+        }
+    });
+    
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert(response.statusText);
+      }
+};
+
+document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
